cf22162c76908fa5a47a0d1b875bfe03c44f51a1
elega <445092967@qq.com>
2016-12-10 22:06:28 -0500

    Update main.py

--- application/main.py
+++ application/main.py
@@ -363,7 +363,7 @@ def register():
     pw_hash = generate_password_hash(password)
     conn = "database"
     try:
-        conn = psycopg2.connect("dbname='stock' user='' host='localhost' password='' ")
+        conn = psycopg2.connect("dbname='stock' user='Linnan' host='localhost' password='' ")
         cur = conn.cursor()
         query = "INSERT INTO user_info (name, pass) VALUES ('{}', '{}');".format(username, pw_hash)
         cur.execute(query)
@@ -389,7 +389,7 @@ def del_user():
     username = request.form['username']
     conn = "database"
     try:
-        conn = psycopg2.connect("dbname='stock' user='' host='localhost' password='' ")
+        conn = psycopg2.connect("dbname='stock' user='Linnan' host='localhost' password='' ")
         cur = conn.cursor()
         query = "DELETE FROM user_info WHERE name='{}';".format(username)
         cur.execute(query)
